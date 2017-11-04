3ac1fdfd2b5bf34eaea614ee9476ad55216023a8
Razwan Rashidi <razwan.izmi@gmail.com>
2017-11-04 23:41:48 +0800

    Create Redux structure

---
+++ frontend/structureStore.js
@@ -0,0 +1,32 @@
+{
+  categories: {
+    "react": "React"
+  },
+  posts: {
+    "8xf0y6ziyjabvozdd253nd": {
+      id: "8xf0y6ziyjabvozdd253nd",
+      "timestamp": 1467166872634,
+      "title": "Udacity is the best place to learn React",
+      "body": "Everyone says so after all.",
+      "author": "thingtwo",
+      "category": "react",
+      "voteScore": 6,
+      "deleted": false,
+      "commentCount": 2
+    }
+  },
+  feed: [],
+  sortBy: "dateDesc",
+  comments: {
+    "894tuq4ut84ut8v4t8wun89g": {
+      "id": "894tuq4ut84ut8v4t8wun89g",
+      "parentId": "8xf0y6ziyjabvozdd253nd",
+      "timestamp": 1468166872634,
+      "body": "Hi there! I am a COMMENT.",
+      "author": "thingtwo",
+      "voteScore": 6,
+      "deleted": false,
+      "parentDeleted": false
+    }
+  }
+}
